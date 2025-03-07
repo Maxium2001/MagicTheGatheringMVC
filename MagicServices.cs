25698df36fb88a3322dcc472a43bf751723f45b7
HasTeriZ <yangweijei@gmail.com>
2025-03-07 16:05:43 +0100

    Add project files.

---
+++ MagicServices.cs
@@ -0,0 +1,12 @@
+﻿using MagicTheGatheringMVC.Models;
+using System.Text.Json;
+
+namespace MagicTheGatheringMVC
+{
+    public class MagicServices
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        private readonly string baseUrl = "https://api.magicthegathering.io/v1/cards";
+    }
+}
---
+++ MagicTheGatheringMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagicTheGatheringMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
